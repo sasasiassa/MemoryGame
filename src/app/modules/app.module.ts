4e2c9fe8fe99636dfc9edc1891c8fc7e344dff6a
sasasiassa <jack20son@gmail.com>
2018-11-21 10:05:43 +0200

    Started project

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { LayoutComponent } from '../components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    
+  LayoutComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
